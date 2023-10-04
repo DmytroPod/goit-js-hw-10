84135409cf0fd66697db3c35d8517c584ec466db
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-10-04 20:27:34 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-// import axios from 'axios';
 import SlimSelect from 'slim-select';
-
+import Notiflix from 'notiflix';
 // axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 const refs = {
@@ -14,41 +13,39 @@ const refs = {
 
 let arrBreeds = [];
 
+refs.breedSelect.addEventListener('change', onSelect);
+
 refs.loader.classList.add('is-hidden');
 refs.error.classList.add('is-hidden');
-refs.breedSelect.classList.add('is-hidden');
+refs.catInfo.classList.add('is-hidden');
 
-fetchBreeds().then(data => {
-  refs.breedSelect.classList.remove('is-hidden');
-  data.forEach(el => {
-    arrBreeds.push({ text: el.name, value: el.id });
+fetchBreeds()
+  .then(data => {
+    data.forEach(el => {
+      arrBreeds.push({ text: el.name, value: el.id });
+    });
+    new SlimSelect({
+      select: refs.breedSelect,
+      data: arrBreeds,
+    });
+  })
+  .catch(err => {
+    console.log(err);
   });
-  new SlimSelect({
-    select: refs.breedSelect,
-    data: arrBreeds,
-  });
-});
-
-refs.breedSelect.addEventListener('change', onSelect);
 
 function onSelect(e) {
   const breedId = e.currentTarget.value;
 
-  refs.loader.classList.remove('is-hidden');
-  refs.breedSelect.classList.add('is-hidden');
+  fetchCatByBreed(breedId)
+    .then(resp => {
+      const { url, breeds } = resp[0];
 
-  console.log(breedId);
-  fetchCatByBreed(breedId).then(resp => {
-    refs.loader.classList.add('is-hidden');
-    refs.breedSelect.classList.remove('is-hidden');
-
-    const { url, breeds } = resp[0];
-
-    refs.catInfo.innerHTML = `
+      return (refs.catInfo.innerHTML = `
       <img src="${url}" width = "500" alt="${breeds[0].name}">
       <h1>${breeds[0].name}</h1>
       <p>${breeds[0].description}</p>
-      <p><span>Temperament:</span> ${breeds[0].temperament}</p>`;
-  });
-  // refs.loader.classList.add('is-hidden');
+      <p><span>Temperament:</span> ${breeds[0].temperament}</p>`);
+    })
+    .catch(err => console.log(err));
+  refs.catInfo.classList.remove('is-hidden');
 }
